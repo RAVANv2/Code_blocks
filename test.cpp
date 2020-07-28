52a6ff4f029bbd7d2fddae08b49f683259536d25
RAVANv2 <chauhanharsh3363334#gmail.com>
2020-07-28 18:56:29 +0530

    new

--- test.cpp
+++ test.cpp
@@ -1,60 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long
 
-bool canWeMakeParatha(int cooks[],int c,int paratha,int mid)
-{
-	int paratha_count = 0;
+bool check(ll int s,ll int e , ll int mid,ll int arr[],ll int k,ll int n){
+    ll int nsum = 0;
+    int no_of_people = 1;  //Initially the number of people required will be 1
+    for(int h =0;h<n;h++){   //No need to update mid
+        if(nsum + arr[h]<= mid){
+            nsum += arr[h];
 
-	for(int i=0;i<c;i++)
-	{
-		int time = 0;
-		for(int p=1;p<=paratha;p++)
-		{
-			if((cooks[i]*p + time) > mid)
-				break;
-			else
-			{
-				time += cooks[i]*p;
-				paratha_count++;
-				if(paratha_count>=paratha)
-					return true;
-			}
-		}
-	}
-	return false;
+        }
+        else{
+            no_of_people++;
+            nsum = arr[h];
+            if(no_of_people>k)  //You have to check this condition here.
+            return false;
+
+        }
+    }
+
+
+    return true;
+
+
+
 
 }
 
+int main() {
+ll int k,n;
+cin>>k>>n;
+ll int sum = 0;
+ll int arr[1000];
+ll int MAx = 0;
+for(int i=0;i<n;i++){
+    cin>>arr[i];
 
-int main()
-{
-	int paratha;
-	cin>>paratha;
-	int c;
-	cin>>c;
-	int cooks[c];
-	for(int i=0;i<c;i++)
-		cin>>cooks[i];
+MAx = max(MAx,arr[i]);
+}
 
-	int s = 0;
-	int e = INT_MAX;
-	int ans=-1;
+for(int j=0;j<n;j++){
+    sum += arr[j];
+}
 
-	while(s<=e)
-	{
-		int mid = (s+e)>>1;
+ll int s = MAx;
+ll int e = sum;
+ll int mid = (s+e)/2;
+ll int ans=s;  //Minimum number of people required would be greater than equal to s, so intialise this value otherwise it will print garbage value if not went into while loop below.
+while(s<=e){
+   mid = (s+e)/2;
+    if(check(s,e,mid,arr,k,n)){
 
-		bool paratha_ready = canWeMakeParatha(cooks,c,paratha,mid);
-
-		if(paratha_ready)
-		{
-			ans = mid;
-			e = mid-1;
-		}
-		else
-			s = mid+1;
-	}
-
-	cout<<ans<<endl;
+        ans = mid;
+          e = mid - 1;
+    }
+    else{
+        s = mid + 1;
+    }
+}
+cout<<ans<<"\n";
 
 }
